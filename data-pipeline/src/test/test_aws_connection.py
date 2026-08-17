e954285af15b3fc2fa6d80bed5ed94382d48befc
mayuribarange368 <mayuribarange368@gmail.com>
2026-08-17 23:23:08 +0530

    Update test_aws_connection.py

--- data-pipeline/src/test/test_aws_connection.py
+++ data-pipeline/src/test/test_aws_connection.py
@@ -1,1 +1,45 @@
+import os
+import boto3
+from dotenv import load_dotenv
 
+def test_aws_connection():
+
+    load_dotenv()
+
+    access_key = os.getenv("AWS_ACCESS_KEY_ID")
+    secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    region = os.getenv("AWS_DEFAULT_REGION")
+    bucket_name = os.getenv("AWS_S3_BUCKET_NAME")
+
+    print("Access Key:", access_key[:4] + "..." if access_key else None)
+    print("Secret Key:", "Loaded" if secret_key else None)
+    print("Region:", repr(region))
+    print("Bucket:", bucket_name)
+
+    print("Testing AWS Connection")
+    print(f"Region: {region}")
+    print(f"Bucket: {bucket_name}")
+
+    try:
+        # Create S3 Client
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id = access_key,
+            aws_secret_access_key = secret_key,
+            region_name=region
+        )
+
+        # List your buckets
+        buckets =  s3.list_buckets()
+        print(f"Connected! You have {len(buckets["Buckets"])} S3 buckets")
+
+
+        return True
+    
+    except Exception as e:
+        print(f"Connection Failed: {e}")
+        print("Check you .env file and aws credentials")
+        return False
+    
+if __name__ == "__main__":
+    test_aws_connection()
